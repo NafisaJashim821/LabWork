13750f8cae3e86e4879d9d8269baf14cebb24816
nafisa <nafisa@jashim.com>
2024-05-26 10:46:27 +0600

    1st question done

--- src/PaymentSystem/PaymentProcess.java
+++ src/PaymentSystem/PaymentProcess.java
@@ -7,14 +7,27 @@ public class PaymentProcess {
     public void setPaymentStrategy(PaymentSystem payment) {
         this.payment = payment;
     }
+
     public boolean makePayment(double amount) {
         if (payment == null) {
             throw new IllegalStateException("Payment strategy not set");
         }
         return payment.pay(amount);
     }
+
     public static void main(String[] args) {
-        PaymentSystem payment = new PaymentSystem();
+        PaymentProcess paymentProcess = new PaymentProcess();
+
+
+        paymentProcess.setPaymentStrategy(new Bank());
+        System.out.println("Bank  " + paymentProcess.makePayment(200));
+
+        paymentProcess.setPaymentStrategy(new Bkash());
+        System.out.println("Bkash payment  " +paymentProcess.makePayment(500);
+
+        paymentProcess.setPaymentStrategy(new CreditCardPayment());
+        System.out.println("CreditCard : " + paymentProcess.makePayment(5500));
 
 
     }
+}
